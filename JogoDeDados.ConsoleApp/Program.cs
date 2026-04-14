1f8edc10528a8e2386d4d3661d20ac26c80b820e
Vitor Caitano <caitanoovitor@gmail.com>
2026-04-14 15:54:37 -0300

    Inicio do projeto do jogo

--- JogoDeDados.ConsoleApp/Program.cs
+++ JogoDeDados.ConsoleApp/Program.cs
@@ -1,1 +1,17 @@
-﻿
+﻿while (true)
+{
+    Console.Clear();
+
+    Console.WriteLine("-------------------------------------");
+    Console.WriteLine("JOGO DOS DADOS");
+    Console.WriteLine("-------------------------------------");
+
+    Console.WriteLine("Deseja continuar? (s/N): ");
+    string? opcaoContinuar = Console.ReadLine()?.ToUpper();
+
+    if (opcaoContinuar != "S")
+        break;
+
+
+
+}
