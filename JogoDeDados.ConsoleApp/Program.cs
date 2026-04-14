3067e345a7f7d46236bd7922ba89a5c09f5609b7
Vitor Caitano <caitanoovitor@gmail.com>
2026-04-14 17:02:28 -0300

    Imprelenteção do gerador de números aleatorios

--- JogoDeDados.ConsoleApp/Program.cs
+++ JogoDeDados.ConsoleApp/Program.cs
@@ -1,10 +1,38 @@
-﻿while (true)
+﻿using System.Security.Cryptography;
+
+const int limiteLinhaChegada = 30;
+int posicaoJogador = 0;
+
+while (true)
 {
     Console.Clear();
 
     Console.WriteLine("-------------------------------------");
     Console.WriteLine("JOGO DOS DADOS");
     Console.WriteLine("-------------------------------------");
+    Console.WriteLine("Rodada do Jogador");
+    Console.WriteLine("-------------------------------------");
+
+    Console.Write("Precione ENTER para lançar o DADO...");
+    Console.ReadLine();
+
+    int resultadoJogador = RandomNumberGenerator.GetInt32(1, 7);
+
+    posicaoJogador += resultadoJogador;
+
+    Console.WriteLine("-------------------------------------");
+    Console.WriteLine("O numero do resultado foi: " + resultadoJogador);
+    Console.WriteLine("-------------------------------------");
+
+    Console.WriteLine($"Você esta na posição: {posicaoJogador} de {limiteLinhaChegada}");
+
+    if (posicaoJogador >= limiteLinhaChegada)
+    {
+        Console.WriteLine($"Você alcancou a linhda de chegada! ");
+        Console.WriteLine("-------------------------------------");
+        Console.Write("Precione ENTER para continuar...");
+        Console.ReadLine();
+    }
 
     Console.WriteLine("Deseja continuar? (s/N): ");
     string? opcaoContinuar = Console.ReadLine()?.ToUpper();
