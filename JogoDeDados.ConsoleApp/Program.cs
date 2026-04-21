43ae1eb81617e9984ca312f029f2802bdef6e723
Vitor Caitano <caitanoovitor@gmail.com>
2026-04-21 20:16:13 -0300

    Implementação da jogada do computador

--- JogoDeDados.ConsoleApp/Program.cs
+++ JogoDeDados.ConsoleApp/Program.cs
@@ -8,7 +8,7 @@ while (true)
 {
     Console.Clear();
     int posicaoJogador = 0;
-
+    int posicaoComputador = 0;
     while (true)
     {
         Console.Clear();
@@ -35,13 +35,17 @@ while (true)
         if (posicaoJogador == 5 || posicaoJogador == 10 || posicaoJogador == 15 || posicaoJogador == 25)
         {
             Console.WriteLine($"\nBônus: Avanço de {bonusDeAvancoExtra} casas");
+
             posicaoJogador += bonusDeAvancoExtra;
+
             Console.WriteLine($"\nVocê esta na posição: {posicaoJogador} de {limiteLinhaChegada}");
         }
-        else if (posicaoJogador == 7 || posicaoJogador == 13 || posicaoJogador == 20)
+        else if (posicaoJogador == 7 || posicaoJogador == 13 || posicaoJogador == 20 || posicaoJogador == 28)
         {
             Console.WriteLine($"\nEvento: recuo de {recuoDePosicao} casas");
+
             posicaoJogador -= recuoDePosicao;
+
             Console.WriteLine($"\nVocê esta na posição: {posicaoJogador} de {limiteLinhaChegada}");
         }
 
@@ -49,6 +53,61 @@ while (true)
         {
             Console.WriteLine("-------------------------------------");
             Console.WriteLine($"Você alcancou a linhda de chegada! ");
+            Console.WriteLine("-------------------------------------");
+            Console.Write("Precione ENTER para continuar...");
+            Console.ReadLine();
+
+            break;
+        }
+
+        Console.WriteLine("-------------------------------------");
+        Console.Write("Precione ENTER para continuar...");
+        Console.ReadLine();
+
+
+        Console.Clear();
+
+        Console.WriteLine("-------------------------------------");
+        Console.WriteLine("JOGO DOS DADOS");
+        Console.WriteLine("-------------------------------------");
+        Console.WriteLine("Rodada do Computador");
+        Console.WriteLine("-------------------------------------");
+
+        Console.Write("Precione ENTER para o computador lançar o DADO...");
+
+        int resultadoComputador = RandomNumberGenerator.GetInt32(1, 7);
+
+        posicaoComputador += resultadoComputador;
+
+        Thread.Sleep(1000);
+
+        Console.WriteLine("-------------------------------------");
+        Console.WriteLine("O numero do resultado do computador foi: " + resultadoComputador);
+        Console.WriteLine("-------------------------------------");
+
+        Console.WriteLine($"O computador esta na posição: {posicaoComputador} de {limiteLinhaChegada}");
+
+        if (posicaoComputador == 5 || posicaoComputador == 10 || posicaoComputador == 15 || posicaoComputador == 25)
+        {
+            Console.WriteLine($"\nBônus: Avanço de {bonusDeAvancoExtra} casas");
+
+            posicaoComputador += bonusDeAvancoExtra;
+
+            Console.WriteLine($"\nO computador esta na posição: {posicaoComputador} de {limiteLinhaChegada}");
+        }
+        else if (posicaoComputador == 7 || posicaoComputador == 13 || posicaoComputador == 20 || posicaoComputador == 28)
+        {
+            Console.WriteLine($"\nEvento: recuo de {recuoDePosicao} casas");
+
+            posicaoComputador -= recuoDePosicao;
+
+            Console.WriteLine($"\nO computador esta na posição: {posicaoComputador} de {limiteLinhaChegada}");
+        }
+
+        if (posicaoComputador >= limiteLinhaChegada)
+        {
+            Console.WriteLine("-------------------------------------");
+            Console.WriteLine($"Computador alcancou a linhda de chegada! ");
             Console.WriteLine("-------------------------------------");
             Console.Write("Precione ENTER para continuar...");
             Console.ReadLine();
