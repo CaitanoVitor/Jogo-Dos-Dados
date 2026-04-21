c038ace992fed0b515490a62d70724acb414d775
Vitor Caitano <caitanoovitor@gmail.com>
2026-04-21 11:11:10 -0300

    Condições de pontuação

--- JogoDeDados.ConsoleApp/Program.cs
+++ JogoDeDados.ConsoleApp/Program.cs
@@ -1,38 +1,68 @@
 ﻿using System.Security.Cryptography;
 
 const int limiteLinhaChegada = 30;
-int posicaoJogador = 0;
+const int bonusDeAvancoExtra = 3;
+const int recuoDePosicao = 2;
 
 while (true)
 {
     Console.Clear();
+    int posicaoJogador = 0;
 
-    Console.WriteLine("-------------------------------------");
-    Console.WriteLine("JOGO DOS DADOS");
-    Console.WriteLine("-------------------------------------");
-    Console.WriteLine("Rodada do Jogador");
-    Console.WriteLine("-------------------------------------");
-
-    Console.Write("Precione ENTER para lançar o DADO...");
-    Console.ReadLine();
-
-    int resultadoJogador = RandomNumberGenerator.GetInt32(1, 7);
-
-    posicaoJogador += resultadoJogador;
-
-    Console.WriteLine("-------------------------------------");
-    Console.WriteLine("O numero do resultado foi: " + resultadoJogador);
-    Console.WriteLine("-------------------------------------");
-
-    Console.WriteLine($"Você esta na posição: {posicaoJogador} de {limiteLinhaChegada}");
-
-    if (posicaoJogador >= limiteLinhaChegada)
+    while (true)
     {
-        Console.WriteLine($"Você alcancou a linhda de chegada! ");
+        Console.Clear();
+
+        Console.WriteLine("-------------------------------------");
+        Console.WriteLine("JOGO DOS DADOS");
+        Console.WriteLine("-------------------------------------");
+        Console.WriteLine("Rodada do Jogador");
+        Console.WriteLine("-------------------------------------");
+
+        Console.Write("Precione ENTER para lançar o DADO...");
+        Console.ReadLine();
+
+        int resultadoJogador = RandomNumberGenerator.GetInt32(1, 7);
+
+        posicaoJogador += resultadoJogador;
+
+        Console.WriteLine("-------------------------------------");
+        Console.WriteLine("O numero do resultado foi: " + resultadoJogador);
+        Console.WriteLine("-------------------------------------");
+
+        Console.WriteLine($"Você esta na posição: {posicaoJogador} de {limiteLinhaChegada}");
+
+        if (posicaoJogador == 5 || posicaoJogador == 10 || posicaoJogador == 15 || posicaoJogador == 25)
+        {
+            Console.WriteLine($"\nBônus: Avanço de {bonusDeAvancoExtra} casas");
+            posicaoJogador += bonusDeAvancoExtra;
+            Console.WriteLine($"\nVocê esta na posição: {posicaoJogador} de {limiteLinhaChegada}");
+        }
+        else if (posicaoJogador == 7 || posicaoJogador == 13 || posicaoJogador == 20)
+        {
+            Console.WriteLine($"\nEvento: recuo de {recuoDePosicao} casas");
+            posicaoJogador -= recuoDePosicao;
+            Console.WriteLine($"\nVocê esta na posição: {posicaoJogador} de {limiteLinhaChegada}");
+        }
+
+        if (posicaoJogador >= limiteLinhaChegada)
+        {
+            Console.WriteLine("-------------------------------------");
+            Console.WriteLine($"Você alcancou a linhda de chegada! ");
+            Console.WriteLine("-------------------------------------");
+            Console.Write("Precione ENTER para continuar...");
+            Console.ReadLine();
+
+            break;
+        }
+
         Console.WriteLine("-------------------------------------");
         Console.Write("Precione ENTER para continuar...");
         Console.ReadLine();
+
+
     }
+
 
     Console.WriteLine("Deseja continuar? (s/N): ");
     string? opcaoContinuar = Console.ReadLine()?.ToUpper();
